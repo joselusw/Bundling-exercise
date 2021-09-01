403e55157b17944f33ade40b7b64507847c73839
JoseluGallardo <jose.gallardo994@gmail.com>
2021-09-01 13:23:43 +0200

    Create bundle

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/style.scss";
+
+export const App = () => {
+	return (
+		<div>
+			<h1>José Luis del Pino Gallardo - Bundling exercise</h1>
+			<img src="https://avatars.githubusercontent.com/u/7702396?s=280&v=4" />
+		</div>
+	);
+};
